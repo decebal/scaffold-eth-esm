d0e4503d1909aa997178bb25113ad2b78396a34f
Decebal Dobrica <352761+decebal@users.noreply.github.com>
2024-09-23 17:48:26 +0100

    chore: fix hooks

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,16 +1,16 @@
 const path = require("path");
 
 const buildNextEslintCommand = (filenames) =>
-  `pnpm run --filter=next:lint --fix --file ${filenames
-    .map((f) => path.relative(path.join("packages", "nextjs"), f))
-    .join(" --file ")}`;
+    `pnpm run --filter=next:lint -- --fix --file ${filenames
+        .map((f) => path.relative(path.join("packages", "nextjs"), f))
+        .join(" --file ")}`;
 
 const checkTypesNextCommand = () => "pnpm run --filter=next:check-types";
 
 const buildHardhatEslintCommand = (filenames) =>
-  `pnpm run --filter=hardhat:lint-staged --fix ${filenames
-    .map((f) => path.relative(path.join("packages", "hardhat"), f))
-    .join(" ")}`;
+    `pnpm run --filter=hardhat:lint-staged -- --fix ${filenames
+        .map((f) => path.relative(path.join("packages", "hardhat"), f))
+        .join(" ")}`;
 
 module.exports = {
   "packages/nextjs/**/*.{ts,tsx}": [
